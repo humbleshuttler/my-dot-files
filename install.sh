7cfdf6b7ad788b9c06e959d6716bcb59d9a71d74
Subrat Prasad <subratkprasad@gmail.com>
2019-08-26 15:10:47 -0700

    Add vim and tmux conf

--- install.sh
+++ install.sh
@@ -6,4 +6,10 @@ set -e
 cp -r dotoh-my-zsh ${HOME}/.oh-my-zsh
 cp ./dotzshrc ${HOME}/.zshrc
 
+cp ./dotvimrc ${HOME}/.vimrc
+cp ./dotvim ${HOME}/.vim
+
+cp ./dottmux.conf ${HOME}/.tmux.conf
+cp ./dottmux-plugins ${HOME}/.tmux-plugins
+
 source ${HOME}/.zshrc
