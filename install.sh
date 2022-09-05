72b5c971e696117f5cbd6f6beecd46b92df06575
Subrat Prasad <subratkprasad@gmail.com>
2022-09-04 22:20:58 -0700

    update

--- install.sh
+++ install.sh
@@ -9,9 +9,6 @@ fi
 # install zsh
 sudo apt install -y zsh
 
-# install powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 working_dir=${PWD}
 
 ln -sv ${working_dir}/dotoh-my-zsh ${HOME}/.oh-my-zsh
@@ -24,6 +21,9 @@ ln -sv ${working_dir}/dottmux.conf ${HOME}/.tmux.conf
 ln -sv ${working_dir}/dottmux-plugins ${HOME}/.tmux-plugins
 ln -sv ${working_dir}/dotgitconfig ${HOME}/.gitconfig
 ln -sv ${working_dir}/dotp10k.zsh ${HOME}/.p10k.sh
+
+# install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 
 source ${HOME}/.zshrc
