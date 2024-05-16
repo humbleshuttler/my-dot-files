cece212d243dc6e8106ab52020f80e2d496437d3
Subrat Prasad <subratkprasad@gmail.com>
2024-05-16 17:47:39 -0400

    update and fix install command

--- install.sh
+++ install.sh
@@ -1,65 +1,80 @@
 #!/bin/bash
 
-c_dir=`basename "$PWD"`
+c_dir=$(basename "$PWD")
 if [ "$c_dir" != "my-dot-files" ]; then
-	echo "run install.sh from root directory"
-	exit 1
+    echo "Run install.sh from the root directory"
+    exit 1
 fi
 
 APT=/usr/bin/apt
 DNF=/usr/bin/dnf
 
-# install zsh
-if [ -f "$APT" ]; then
-    echo "found apt. Installing zsh"
-    sudo apt install -y zsh
-fi
-
-if [ -f "$DNF" ]; then
-    echo "Found dnf. Installing dnf"
-    sudo dnf install -y zsh
+# Install Zsh if it's not already installed
+if ! command -v zsh >/dev/null 2>&1; then
+    if [ -f "$APT" ]; then
+        echo "Found apt. Installing zsh"
+        sudo apt update
+        sudo apt install -y zsh
+    elif [ -f "$DNF" ]; then
+        echo "Found dnf. Installing zsh"
+        sudo dnf install -y zsh
+    fi
+else
+    echo "Zsh is already installed"
 fi
 
 # Check if tmux is already installed
-if ! command -v tmux >/dev/null; then
+if ! command -v tmux >/dev/null 2>&1; then
+    # Install tmux
+    if [ -f "$APT" ]; then
+        sudo apt update
+        sudo apt install -y tmux
+    elif [ -f "$DNF" ]; then
+        sudo dnf install -y tmux
+    fi
 
-  # Install tmux
-  sudo apt update
-  sudo apt install tmux
-
-  # Tell the user that tmux has been installed
-  echo "tmux has been installed."
+    # Tell the user that tmux has been installed
+    echo "tmux has been installed."
+else
+    echo "tmux is already installed"
 fi
 
 # Check if ctags is already installed
-if ! command -v ctags >/dev/null; then
+if ! command -v ctags >/dev/null 2>&1; then
+    # Install universal-ctags or fall back to exuberant-ctags
+    if [ -f "$APT" ]; then
+        sudo apt update
+        if apt-cache show universal-ctags >/dev/null 2>&1; then
+            sudo apt install -y universal-ctags
+        else
+            sudo apt install -y exuberant-ctags
+        fi
+    elif [ -f "$DNF" ]; then
+        sudo dnf install -y ctags
+    fi
 
-  # Install ctags
-  sudo apt update
-  sudo apt install ctags
-
-  # Tell the user that ctags has been installed
-  echo "ctags has been installed."
+    # Tell the user that ctags has been installed
+    echo "ctags has been installed."
+else
+    echo "ctags is already installed"
 fi
 
 working_dir=${PWD}
 
 # Define the create_symlink function
-function create_symlink() {
-  # Get the source and destination paths
-  source_path=$1
-  destination_path=$2
+create_symlink() {
+    local source_path=$1
+    local destination_path=$2
 
-  # Check if the symlink already exists
-  if [ -L "$destination_path" ]; then
-    echo "Symlink ${destination_path} already exists."
-  else
-    # The symlink does not exist, so create it
-    ln -s "$source_path" "$destination_path"
-    echo "Symlink ${destination_path} created."
-  fi
+    # Check if the symlink already exists
+    if [ -L "$destination_path" ]; then
+        echo "Symlink ${destination_path} already exists."
+    else
+        # The symlink does not exist, so create it
+        ln -s "$source_path" "$destination_path"
+        echo "Symlink ${destination_path} created."
+    fi
 }
-
 
 create_symlink ${working_dir}/dotoh-my-zsh ${HOME}/.oh-my-zsh
 create_symlink ${working_dir}/dotzshrc ${HOME}/.zshrc
@@ -71,17 +86,19 @@ create_symlink ${working_dir}/dottmux.conf ${HOME}/.tmux.conf
 create_symlink ${working_dir}/dottmux-plugins ${HOME}/.tmux-plugins
 create_symlink ${working_dir}/dotgitconfig ${HOME}/.gitconfig
 create_symlink ${working_dir}/dotp10k.zsh ${HOME}/.p10k.zsh
-create_symlink ${working_dir}/dev-tools/* /usr/local/bin/
 
-# install powerlevel10k
+# Use sudo to create symbolic links in /usr/local/bin
+for file in ${working_dir}/dev-tools/*; do
+    sudo ln -s "$file" /usr/local/bin/
+done
+
+# Install powerlevel10k
 if ! test -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"; then
-
-  echo "install powerlevel10k theme for zsh"
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    echo "Install powerlevel10k theme for zsh"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 else
-  echo "powerlevel10k already installed"
+    echo "powerlevel10k already installed"
 fi
 
-
-
-source ${HOME}/.zshrc
+# Use Zsh to source the .zshrc file
+zsh -c "source ${HOME}/.zshrc"
